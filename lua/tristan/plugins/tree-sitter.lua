60f42a88514f3efc33e32a3dcd3d63a39e4809f1
TristanC111 <tristan.carbonneau@icloud.com>
2026-01-15 11:32:01 -0800

    fixed treesitter

--- lua/tristan/plugins/tree-sitter.lua
+++ lua/tristan/plugins/tree-sitter.lua
@@ -3,26 +3,31 @@ return {
 	event = { "BufReadPre", "BufNewFile" },
 	build = ":TSUpdate",
 	config = function()
-		-- import nvim-treesitter plugin
-		-- Load the configuration module
 		local treesitter = require("nvim-treesitter.configs")
 
-		-- Call the setup function with your desired options
 		treesitter.setup({
-			-- Add languages to be installed here
-			ensure_installed = { "bash", "c", "lua", "vim", "vimdoc", "markdown" },
+			-- REQUIRED (by Lua-LS typing)
+			modules = {},
+			sync_install = false,
+			ignore_install = {},
 
-			-- Enable syntax highlighting
+			ensure_installed = {
+				"bash",
+				"c",
+				"lua",
+				"vim",
+				"vimdoc",
+				"markdown",
+			},
+
 			highlight = {
 				enable = true,
 			},
 
-			-- Enable indentation
 			indent = {
 				enable = true,
 			},
 
-			-- Autoinstall languages that are not installed
 			auto_install = true,
 		})
 	end,
